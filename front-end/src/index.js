37f443e07d0a8d6f292b732eb43bfd65df677b3e
Jason Luo <jasonl960523@gmail.com>
2017-12-18 23:08:59 +0800

    Set up the navbar

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className='gallery'></div>
+        <div className='nav'>
+          <div className='artist-name'>YUEN PIK KWAN</div>
+          <div className='artist-title'>Metal Maker & Multimedia Designer</div>
+          <ul className='navbar'>
+            <li className='nav-item'>
+              <a>Blog</a>
+            </li>
+            <li className='nav-item'>
+              <a>Crafts</a>
+            </li>
+            <li className='nav-item'>
+              <a>Design</a>
+            </li>
+            <li className='nav-item'>
+              <a>About</a>
+            </li>
+            <li className='nav-item'>
+              <a>Contact</a>
+            </li>
+          </ul>
+          <div className='nav-deco'>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
