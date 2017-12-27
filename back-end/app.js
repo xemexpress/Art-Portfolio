380f4cdb587a64fea16f6489cca5a8c789334230
Jason Luo <jasonl960523@gmail.com>
2017-12-27 16:25:58 +0800

    Fixes on initialisation (Part III)
    Includes:
      - Restructure app.js
      - Remove www
      - Uninstall some unused packages from package.json

--- back-end/app.js
+++ back-end/app.js
@@ -1,29 +1,41 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var fs = require('fs'),
+    http = require('http'),
+    path = require('path'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    cors = require('cors'),
+    passport = require('passport'),
+    errorhandler = require('errorhandler'),
+    mongoose = require('mongoose');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var isProduction = process.env.NODE_ENV === 'production'
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(cors());
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(require('morgan')('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+app.use(require('method-override')());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(session({ secret: 'art-portfolio', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if(!isProduction){
+  app.use(errorhandler());
+}
+
+if(isProduction){
+  mongoose.connect(process.env.MONGODB_URI);
+}else{
+  mongoose.connect('mongodb://localhost/art-portfolio');
+  mongoose.set('debug', true);
+}
+
+app.use(require('./routes'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,14 +45,26 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// development mode: reveal errors to developers
+if(!isProduction){
+  
+}
 
-module.exports = app;
+app.use(function(err, req, res, next){
+  console.log(err.stack)
+
+  res.status(err.status || 500)
+
+  // no stacktrace leaked to user
+  res.json({
+    'errors': {
+      'message': err.message,
+      error: isProduction ? {} : err
+    }
+  })
+})
+
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log('Listening on port ' + server.address().port)
+})
