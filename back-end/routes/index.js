316565e319198cb2086697c708be0cf61b91a7de
Jason Luo <jasonl960523@gmail.com>
2017-12-30 16:37:55 +0800

    Set up the error handler middleware

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -3,4 +3,17 @@ var router = express.Router();
 
 router.use('/api', require('./api'));
 
+router.use(function(err, req, res, next){
+  if(err.name === 'ValidationError'){
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduece(function(errors, key){
+        errors[key] = err.errors[key].message
+        return errors
+      })
+    })
+  }
+
+  return next(err)
+})
+
 module.exports = router;
