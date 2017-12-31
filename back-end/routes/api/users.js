89c3c5bb7b3e85e2039ac12511d7b9fbe64e0b83
Jason Luo <jasonl960523@gmail.com>
2017-12-31 16:22:28 +0800

    Create the login route

--- back-end/routes/api/users.js
+++ back-end/routes/api/users.js
@@ -4,8 +4,8 @@ var passport = require('passport')
 var User = mongoose.model('User')
 var auth = require('../auth')
 
+// Admin initialization
 router.post('/users', function(req, res, next){
-  // Admin initialization
   if(req.body.username === auth.admin){
     var user = new User()
     user.username = req.body.username
@@ -17,6 +17,27 @@ router.post('/users', function(req, res, next){
   }else{
     return res.status(401).json({ errors: { 'admin': 'is invalid' } })
   }
+})
+
+// Admin login
+router.post('/users/login', function(req, res, next){
+  if(!req.body.username){
+    res.status(422).json({ errors: { username: "can't be blank" } })
+  }
+
+  if(!req.body.password){
+    res.status(422).json({ errors: { password: "can't be blank" } })
+  }
+
+  passport.authenticate('local', { session: false }, function(err, user, info){
+    if(err){ return next(err) }
+
+    if(user){
+      return res.json({ user: user.toAuthJSON })
+    }else{
+      return res.status(422).json(info)
+    }
+  })(req, res, next)
 })
 
 module.exports = router
