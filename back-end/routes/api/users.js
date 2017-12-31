16932495a129bf5276418198e9c935af50425136
Jason Luo <jasonl960523@gmail.com>
2017-12-31 16:34:00 +0800

    Create the getCurrentUser route

--- back-end/routes/api/users.js
+++ back-end/routes/api/users.js
@@ -33,11 +33,20 @@ router.post('/users/login', function(req, res, next){
     if(err){ return next(err) }
 
     if(user){
-      return res.json({ user: user.toAuthJSON })
+      return res.json({ user: user.toAuthJSON() })
     }else{
       return res.status(422).json(info)
     }
   })(req, res, next)
+})
+
+// Get the current user
+router.get('/user', auth.required, function(req, res, next){
+  User.findById(req.payload.id).then(function(user){
+    if(!user){ return res.sendStatus(401)}
+
+    return res.json({ user: user.toAuthJSON() })
+  }).catch(next)
 })
 
 module.exports = router
