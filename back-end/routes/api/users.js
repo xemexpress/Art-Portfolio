eb4de0edcf517ae339af3addfa689b7f50595831
Jason Luo <jasonl960523@gmail.com>
2018-01-03 16:42:49 +0800

    Fixes (minor)

--- back-end/routes/api/users.js
+++ back-end/routes/api/users.js
@@ -45,7 +45,7 @@ router.post('/users/login', function(req, res, next){
 // Get the current user
 router.get('/user', auth.required, function(req, res, next){
   User.findById(req.payload.id).then(function(user){
-    if(!user){ return res.sendStatus(401)}
+    if(!user){ return res.sendStatus(401) }
 
     return res.json({ user: user.toAuthJSON() })
   }).catch(next)
