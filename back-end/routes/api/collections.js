6e66a9848d25fd3d66891eaed35b926d2817ddca
Jason Luo <jasonl960523@gmail.com>
2018-01-03 16:14:31 +0800

    Build the create collection route and intercept collection data

--- back-end/routes/api/collections.js
+++ back-end/routes/api/collections.js
@@ -2,6 +2,32 @@ var mongoose = require('mongoose')
 // mongoose.Promise = global.Promise
 var router = require('express').Router()
 var Collection = mongoose.model('Collection')
+var User = mongoose.model('User')
 var auth = require('../auth')
+
+router.param('collection', function(req, res,next, slug){
+  Collection.findOne({ slug: slug })
+    .then(function(collection){
+      if(!collection){ return res.sendStatus(404) }
+
+      req.collection = collection
+
+      return next()
+    }).catch(next)
+})
+
+// Create collection
+router.post('/collections', auth.required, function(req, res, next){
+  User.findById(req.payload.id).then(function(user){
+    if(!user){ return res.sendStatus(401) }
+    
+    var collection = new Collection(req.body.collection)
+
+    return collection.save().then(function(){
+      return res.json({ collection: collection.toJSONFor() })
+    })
+  }).catch(next)
+
+})
 
 module.exports = router
