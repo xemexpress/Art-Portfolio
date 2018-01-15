da72fbc9b0221e502e4c4fce8a3f28c946263b7d
Jason Luo <jasonl960523@gmail.com>
2018-01-15 14:40:30 +0800

    CSS modification III

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -19,15 +19,40 @@ class Nav extends React.Component {
               <span className='crafts-drop'>Crafts</span>
               <ul className='crafts'>
                 <span className='nav-hand'></span>
-                <li>Jewlery&nbsp;practice</li>
-                <li>Light&nbsp;up&nbsp;silver&nbsp;jewelry</li>
-                <li>Orgami&nbsp;jewelry&nbsp;series</li>
-                <li>Shell&nbsp;small&nbsp;object</li>
-                <li>Weave&nbsp;jewelry</li>
+                <li><a>Jewlery&nbsp;practice</a></li>
+                <li><a>Light&nbsp;up&nbsp;silver&nbsp;jewelry</a></li>
+                <li><a>Orgami&nbsp;jewelry&nbsp;series</a></li>
+                <li><a>Shell&nbsp;small&nbsp;object</a></li>
+                <li><a>Weave&nbsp;jewelry</a></li>
               </ul>
             </li>
             <li className='category'>
-              Design
+              <span className='design-drop'>Design</span>
+              <ul className='design'>
+                <span className='nav-hand'></span>
+                <li className='sub-category'>
+                  <span className='book-design-drop'>Book&nbsp;design</span>
+                  <ul className='book-design'>
+                    <span className='nav-hand'></span>
+                    <li><a>Ancestral&nbsp;house</a></li>
+                    <li><a>Artist&nbsp;book</a></li>
+                    <li><a>Future&nbsp;calendar</a></li>
+                  </ul>
+                </li>
+                <li className='sub-category'>
+                <span className='brand-design-drop'>Brand&nbsp;design</span>
+                  <ul className='brand-design'>
+                    <span className='nav-hand'></span>
+                    <li><a>Ancestral&nbsp;house</a></li>
+                    <li><a>Artist&nbsp;book</a></li>
+                    <li><a>Future&nbsp;calendar</a></li>
+                  </ul>
+                </li>
+                <li><a>Graphic&nbsp;&amp;&nbsp;sketch</a></li>
+                <li><a>Icon&nbsp;design</a></li>
+                <li><a>Others</a></li>
+                <li><a>UI&nbsp;design</a></li>
+              </ul>
             </li>
             <li className='category'>
               About
