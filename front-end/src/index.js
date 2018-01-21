a4fd2c9771cd8b073cc0eb0e1b51343eb4d5a459
Jason Luo <jasonl960523@gmail.com>
2018-01-21 16:21:52 +0800

    Set up middleware and import it to the store

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './components/App'
+import { promiseMiddleware } from './middleware'
 
 const defaultState = {
   header: 'Yuen Pik Kwan',
@@ -12,16 +13,20 @@ const defaultState = {
   concentration: 'Metal Maker & Multimedia Designer',
   units: null
 }
+
 const reducer = function(state = defaultState, action){
   switch(action.type){
-    case 'TOGGLE':
-      return { ...state, checked: !state.checked }
+    case 'BLOG_PAGE_LOADED':
+      return {
+        ...state,
+        articles: action.payload.articles
+      }
     default:
   }
   return state
 }
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(promiseMiddleware))
 
 ReactDOM.render((
   <Provider store={store}>
