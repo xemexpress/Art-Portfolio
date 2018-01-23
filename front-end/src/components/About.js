1d9acc281f65245f013fc20697b38764f3b16f8d
Jason Luo <jasonl960523@gmail.com>
2018-01-23 16:21:52 +0800

    Update About Component

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -3,7 +3,13 @@ import React from 'react'
 class About extends React.Component {
   render(){
     return (
-      <div>Its about.</div>
+      <div className='about'>
+        <img src='http://res.cloudinary.com/unimemo-dfd94/image/upload/v1516694472/instalgram_h69qvh.svg' alt='instalgram-icon'/>
+        <div className='welcome'>
+          Follow me on Instalgram<br/>
+          If you have any question feel free to contact me!
+        </div>
+      </div>
     )
   }
 }
