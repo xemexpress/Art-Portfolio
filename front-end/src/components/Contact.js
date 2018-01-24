ee71d6350ab3df7e502b92a38a2620e9fc3c5cfa
Jason Luo <jasonl960523@gmail.com>
2018-01-24 17:31:49 +0800

    Update Contact Component

---
+++ front-end/src/components/Contact.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+class About extends React.Component {
+  render(){
+    return (
+      <div className='row'>
+        <div className='contact offset-lg-3 col-lg-6'>
+          <img src='http://res.cloudinary.com/unimemo-dfd94/image/upload/v1516694472/instalgram_h69qvh.svg' alt='instalgram-icon'/>
+          <div className='welcome'>
+            Follow me on Instalgram<br/>
+            If you have any question feel free to contact me!
+          </div>
+          <form>
+            <input
+              className='messager'
+              type='text'
+              placeholder='Your name' /><br/>
+            <textarea
+              className='message'
+              type='text'
+              rows='9'
+              placeholder='Write down something...' />
+            <br/>
+            <button
+              type='submit'>
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default About
