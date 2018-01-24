d8921973072a659c8e011a903e4fbb62147c323f
Jason Luo <jasonl960523@gmail.com>
2018-01-24 21:13:01 +0800

    Add Ig link to icon in contact

--- front-end/src/components/Contact.js
+++ front-end/src/components/Contact.js
@@ -4,8 +4,8 @@ class About extends React.Component {
   render(){
     return (
       <div className='row'>
-        <div className='contact offset-lg-3 col-lg-6'>
-          <img src='http://res.cloudinary.com/unimemo-dfd94/image/upload/v1516694472/instalgram_h69qvh.svg' alt='instalgram-icon'/>
+        <div className='contact offset-lg-3 col-lg-6 offset-xs-2 col-xs-10'>
+          <a href='https://www.instagram.com/kate.redtea.cake/' target='blank'><img src='http://res.cloudinary.com/unimemo-dfd94/image/upload/v1516694472/instalgram_h69qvh.svg' alt='instalgram-icon'/></a>
           <div className='welcome'>
             Follow me on Instalgram<br/>
             If you have any question feel free to contact me!
