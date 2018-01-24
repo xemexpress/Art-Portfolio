536f20b642fb9b99992fd4ff83950f6ac86177e8
Jason Luo <jasonl960523@gmail.com>
2018-01-24 19:06:28 +0800

    Create Links for entries in Nav

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,29 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 class Nav extends React.Component {
   render(){
     return (
       <div className='col-lg-4 col-md-5'>
         <div className='nav'>
-          <div className='artist-name'>
-            {this.props.header.toUpperCase()}
-          </div>
+          <Link to='/' className='link'>
+            <div className='artist-name'>
+              {this.props.header.toUpperCase()}
+            </div>
+          </Link>
           <div className='artist-title'>
             {this.props.concentration}
           </div>
           <ul className='navbar'>
             <li className='category'>
-              Blog
+              <Link to='/' className='link'>
+                Blog
+              </Link>
             </li>
             <li className='category'>
               <span className='crafts-drop'>Crafts</span>
               <ul className='crafts'>
                 <span className='nav-hand'></span>
-                <li><a>Jewlery&nbsp;practice</a></li>
-                <li><a>Light&nbsp;up&nbsp;silver&nbsp;jewelry</a></li>
-                <li><a>Orgami&nbsp;jewelry&nbsp;series</a></li>
-                <li><a>Shell&nbsp;small&nbsp;object</a></li>
-                <li><a>Weave&nbsp;jewelry</a></li>
+                <li><Link to='gallery/jewlery-practice' className='link'>Jewlery&nbsp;practice</Link></li>
+                <li><Link to='gallery/light-up-silver-jewelry' className='link'>Light&nbsp;up&nbsp;silver&nbsp;jewelry</Link></li>
+                <li><Link to='gallery/origami-jewelry-series' className='link'>Origami&nbsp;jewelry&nbsp;series</Link></li>
+                <li><Link to='gallery/shell-small-object' className='link'>Shell&nbsp;small&nbsp;object</Link></li>
+                <li><Link to='gallery/weave-jewelry' className='link'>Weave&nbsp;jewelry</Link></li>
               </ul>
             </li>
             <li className='category'>
@@ -34,31 +39,35 @@ class Nav extends React.Component {
                   <span className='book-design-drop'>Book&nbsp;design</span>
                   <ul className='book-design'>
                     <span className='nav-hand'></span>
-                    <li><a>Ancestral&nbsp;house</a></li>
-                    <li><a>Artist&nbsp;book</a></li>
-                    <li><a>Future&nbsp;calendar</a></li>
+                    <li><Link to='gallery/ancestral-house' className='link'>Ancestral&nbsp;house</Link></li>
+                    <li><Link to='gallery/artist-book' className='link'>Artist&nbsp;book</Link></li>
+                    <li><Link to='gallery/future-calendar' className='link'>Future&nbsp;calendar</Link></li>
                   </ul>
                 </li>
                 <li className='sub-category'>
                 <span className='brand-design-drop'>Brand&nbsp;design</span>
                   <ul className='brand-design'>
                     <span className='nav-hand'></span>
-                    <li><a>Ancestral&nbsp;house</a></li>
-                    <li><a>Artist&nbsp;book</a></li>
-                    <li><a>Future&nbsp;calendar</a></li>
+                    <li><Link to='gallery/herbal-day' className='link'>Herbal&nbsp;day</Link></li>
+                    <li><Link to='gallery/mindtree' className='link'>Mindtree</Link></li>
+                    <li><Link to='gallery/sound' className='link'>Sound</Link></li>
+                    <li><Link to='gallery/stationary' className='link'>Stationary</Link></li>
+                    <li><Link to='gallery/tongxianghui' className='link'>Tongxianghui</Link></li>
                   </ul>
                 </li>
-                <li><a>Graphic&nbsp;&amp;&nbsp;sketch</a></li>
-                <li><a>Icon&nbsp;design</a></li>
-                <li><a>Others</a></li>
-                <li><a>UI&nbsp;design</a></li>
+                <li><Link to='gallery/graphic-sketch' className='link'>Graphic&nbsp;&amp;&nbsp;sketch</Link></li>
+                <li><Link to='gallery/icon-design' className='link'>Icon&nbsp;design</Link></li>
+                <li><Link to='gallery/ui-design' className='link'>UI&nbsp;design</Link></li>
+                <li><Link to='gallery/others' className='link'>Others</Link></li>
               </ul>
             </li>
             <li className='category'>
               About
             </li>
             <li className='category'>
-              Contact
+              <Link to='contact' className='link'>
+                Contact
+              </Link>
             </li>
           </ul>
           <div className='nav-deco'>
