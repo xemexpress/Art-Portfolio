436f291198d37d2c3671ebee059d3a72109dd60c
Jason Luo <jasonl960523@gmail.com>
2018-01-25 23:11:14 +0800

    Update gallery's reducer

--- front-end/src/reducers/gallery.js
+++ front-end/src/reducers/gallery.js
@@ -1,3 +1,11 @@
 export default (state={}, action) => {
+  switch(action.type){
+    case 'GALLERY_PAGE_LOADED':
+      return {
+        ...state,
+        units: action.payload.units
+      }
+    default:
+  }
   return state
 }
