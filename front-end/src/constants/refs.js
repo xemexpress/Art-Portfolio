5422722f082ec9c2cebd27451c5bf08234292148
Jason Luo <jasonl960523@gmail.com>
2018-01-30 12:30:25 +0800

    Update general refs constants

--- front-end/src/constants/refs.js
+++ front-end/src/constants/refs.js
@@ -1,7 +1,7 @@
 export const API_ROOT = 'http://localhost:3000/api'
 
-export const HEADER = 'Yuen Pik Kwan'
-export const ARTIST = 'Kate Yuen'
-export const CONCENTRATION = 'Metal Maker & Multimedia Designer'
+export const HEADER = 'Header'
+export const ARTIST = 'The Artist'
+export const CONCENTRATION = 'The Artist\'s Title'
 
 export const PER_PAGE = 15
